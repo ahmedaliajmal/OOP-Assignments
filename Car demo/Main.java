d10a1f911b2aa9725a05c70219835a3cf95c39b5
Ahmed Ali <147745572+ahmedaliajmal@users.noreply.github.com>
2026-07-24 20:39:42 +0500

    Add files via upload

---
+++ Car demo/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car myCar = new Car();
+
+        myCar.displayState();
+
+        myCar.setDetails("Toyota", "Red");
+
+        myCar.startEngine();
+
+        myCar.displayState();
+
+        myCar.stopEngine();
+
+        myCar.displayState();
+    }
+}
