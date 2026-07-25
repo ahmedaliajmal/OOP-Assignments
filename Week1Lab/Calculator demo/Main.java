110aa549db4eb8d081986bb0d706994200987b2d
Ahmed Ali <147745572+ahmedaliajmal@users.noreply.github.com>
2026-07-25 19:01:09 +0500

    Add files via upload

---
+++ Week1Lab/Calculator demo/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println("Add: " + calc.add(5, 3));
+        System.out.println("Subtract: " + calc.subtract(5, 3));
+        System.out.println("Multiply: " + calc.multiply(5, 3));
+        System.out.println("Divide: " + calc.divide(6, 3));
+
+        
+        System.out.println("Divide by zero test: " + calc.divide(10, 0));
+    }
+}
